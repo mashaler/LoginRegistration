92ab26bb4bd54f76c8f5ca518706a71e8a04e2d3
mashaler <mashalerolva@gmail.com>
2023-02-11 18:38:27 +0200

    updated

--- signin.py
+++ signin.py
@@ -1,7 +1,95 @@
 from tkinter import *
 from PIL import ImageTk
+from tkinter import messagebox
+import pymysql
 
 # Functionality Part
+def forget_pass():
+    def change_password():
+        if user_Entry.get() == '' or newpass_Entry.get() =='' or confirmpass_Entry.get()=='':
+            messagebox.showerror('Error','All Fields Are Required', parent=window)
+        elif newpass_Entry.get() != confirmpass_Entry.get():
+            messagebox.showerror('Error','Password amd confirm password are not matching', parent=window)
+        else:
+            con = pymysql.connect(host='localhost',user='root', password='root', database='userdata')
+            mycursor = con.cursor()
+            query = 'select * from data where username=%s'
+            mycursor.execute(query,(user_Entry.get()))
+            row = mycursor.fetchone()
+            if row ==None:
+                messagebox.showerror('Error','Incorrect Username',parent=window)
+            else:
+                query = 'update data set password=%s where username=%s'
+                mycursor.execute(query,(newpass_Entry.get(), user_Entry.get()))
+                con.commit()
+                con.close()
+                messagebox.showinfo('Success','Password is reset, Please login with new password', parent=window)
+                window.destroy()
+
+    window = Toplevel()
+    window.title('Change Password')
+
+    bgPic = ImageTk.PhotoImage(file='./loginsignuppage/background.jpg')
+    bgLabel = Label(window, image=bgPic)
+    bgLabel.grid()
+
+    heading_label = Label(window, text='RESET PASSWORD', font=('arial','18','bold'), bg ='white', fg='magenta2')
+    heading_label.place(x=480, y=60)
+
+    userLabel = Label(window, text='Username', font=('arial',12, 'bold'), bg ='white', fg = 'orchid1')
+    userLabel.place(x=470, y=130)
+
+    user_Entry = Entry(window, width=25, fg='magenta2', font=('arial',11,'bold'), bd=0)
+    user_Entry.place(x=470, y=160)
+
+    Frame(window, width=250, height=2, bg='orchid1').place(x=470, y=180)
+
+    passwordLabel = Label(window, text='New Password', font=('arial','12','bold'), bg ='white', fg='orchid1')
+    passwordLabel.place(x=470, y=210)
+
+    newpass_Entry = Entry(window, width=25, fg='magenta2', font=('arial',11,'bold'), bd=0)
+    newpass_Entry.place(x=470, y=240)
+
+    Frame(window, width=250, height=2, bg='orchid1').place(x=470, y=260)    
+
+    confirmpassLabel = Label(window, text='Confirm Password', font=('arial','11','bold'), bg ='white', fg='orchid1')
+    confirmpassLabel.place(x=470, y=290)
+
+    confirmpass_Entry = Entry(window, width=25, fg='magenta2', font=('arial',11,'bold'), bd=0)
+    confirmpass_Entry.place(x=470, y=320)
+
+    Frame(window, width=250, height=2, bg='orchid1').place(x=470, y=340)
+
+    submitButton = Button(window, text='Submit', bd=0, bg='magenta2', fg='white', font=('Open Sans','16','bold'),
+                                width=19, cursor='hand2', activebackground='magenta2', activeforeground='white',
+                                command=change_password)
+    submitButton.place(x=470, y=390)
+
+
+
+    window.mainloop()
+
+def login_user():
+    if UsernameEntry.get() =='' or passwordEntry.get() == '':
+        messagebox.showerror('Error','All Fields Are Required')
+    else:
+        try:
+            con = pymysql.connect(host='localhost',user='root', password='root')
+            mycursor = con.cursor()
+        except:
+            messagebox.showerror('Error','Connection is not established try again')
+            return
+        query = 'use userdata'
+        mycursor.execute(query)
+        query = 'select * from data where username =%s and password = %s'
+        mycursor.execute(query,(UsernameEntry.get(),passwordEntry.get()))
+        row = mycursor.fetchone()
+
+        if row == None:
+            messagebox.showerror('Error','Invalid username or password')
+        else:
+            messagebox.showinfo('Welcome','Login is Successful')
+
 def signup_page():
     login_windom.destroy()
     import signup
@@ -61,11 +149,11 @@ eyeButton = Button(login_windom,image=openeye,bd=0,bg='white',activebackground='
 eyeButton.place(x=800,y=255)
 
 forgetButton = Button(login_windom,text='Forgot Password?',bd=0,bg='white',activebackground='white',
-                         cursor='hand2',font=('Microsoft Yahei UI LIGHT',9,'bold'),fg='firebrick1',activeforeground='firebrick1')
+                         cursor='hand2',font=('Microsoft Yahei UI LIGHT',9,'bold'),fg='firebrick1',activeforeground='firebrick1',command=forget_pass)
 forgetButton.place(x=715,y=295)
 
 loginButton = Button(login_windom,text='Login',font=('open sans',16,'bold'),fg='white',bg='firebrick1',
-activeforeground='white',activebackground='firebrick1',cursor='hand2',bd=0,width=19)
+activeforeground='white',activebackground='firebrick1',cursor='hand2',bd=0,width=19,command=login_user)
 loginButton.place(x=578,y=350)
 
 orLabel = Label(login_windom,text='---------------OR---------------',font=('open sans',16,'bold'),fg='firebrick1',
